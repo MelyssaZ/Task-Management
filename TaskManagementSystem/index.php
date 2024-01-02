4a9e2de30859f2f02eca4f9994713901ff7de5a5
Melyssa Zarate <101029454+MelyssaZ@users.noreply.github.com>
2024-01-02 10:02:11 -0800

    fix files

--- TaskManagementSystem/index.php
+++ TaskManagementSystem/index.php
@@ -10,15 +10,15 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-    <div class="container">
-        <div class="content">
-            <h2>Login Form</h2>
-            <label for="uID"><b>User ID</b></label>
-            <input type="text" placeholder="Enter User ID" name="uID" required>
-            <label for="pwd"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="pwd" required>
-            <button type="submit">Login</button>
-        </div>
-    </div>
+<h1>Login</h1>
+<form action="auth.php" method="post">
+    <label for="username">Username:</label>
+    <input type="text" name="username" required>
+    <br>
+    <label for="password">Password:</label>
+    <input type="password" name="password" required>
+    <br>
+    <input type="submit" value="Login">
+</form>
 </body>
 </html>
