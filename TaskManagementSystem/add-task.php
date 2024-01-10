b945a79a87a93e5cf26e0954ed2d322dc1dfd171
Melyssa Zarate <101029454+MelyssaZ@users.noreply.github.com>
2024-01-10 12:42:08 -0800

    Update add-task.php

--- TaskManagementSystem/add-task.php
+++ TaskManagementSystem/add-task.php
@@ -1,1 +1,23 @@
 <?php
+include ('config/constants.php');
+?>
+<html>
+<head>
+    <title>Task Manager</title>
+</head>
+<body>
+    <h1>Task Manager</h1>
+    <a href="<?php echo SITEURL; ?>">Home</a>
+    <h3>Add Task Page</h3>
+
+    <form method="post" action="">
+        <table>
+            <tr>
+                <td>Task Name: </td>
+                <td><input type="text" name="task_name" placeholder="Type Task Name" required="required" /></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
+
