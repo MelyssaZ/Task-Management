ee4a3efab2d65b2cb048df29a72b4c463da1d114
Melyssa Zarate <101029454+MelyssaZ@users.noreply.github.com>
2024-01-10 16:55:08 -0800

    Update add-task.php

--- TaskManagementSystem/add-task.php
+++ TaskManagementSystem/add-task.php
@@ -16,6 +16,29 @@ include ('config/constants.php');
                 <td>Task Name: </td>
                 <td><input type="text" name="task_name" placeholder="Type Task Name" required="required" /></td>
             </tr>
+            <tr>
+                <td>Task Description: </td>
+                <td><textarea name="task_description" placeholder="Type Task Description Here"></textarea></td>
+            </tr>
+            <tr>
+                <td>Select List</td>
+                <td>
+                    <select name="list_id">
+                        <option value="1">To Do</option>
+                        <option value="2">Doing</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>Priority: </td>
+                <td>
+                    <select name="priority">
+                        <option value="High">High</option>
+                        <option value="Medium">Medium</option>
+                        <option value="Low">Low</option>
+                    </select>
+                </td>
+            </tr>
         </table>
     </form>
 </body>
